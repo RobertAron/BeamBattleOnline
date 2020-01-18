4d3f414bbd6630e347ad0a3b8d6fc5f1b48d5126
unknown <Robert.Aron.tx@gmail.com>
2020-01-18 00:09:23 -0600

    seemed to have stopped the crashing

---
+++ Assets/Scripts/WallCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+[System.Obsolete]
+public class WallCollision : NetworkBehaviour
+{
+    [ServerCallback]
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("COLLIDE");
+        NetworkServer.Destroy(other.gameObject);
+    }
+}
