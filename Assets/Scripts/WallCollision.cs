8e04a5d3b9295fa585d95820439e52a255d1f7a3
unknown <Robert.Aron.tx@gmail.com>
2020-01-19 22:30:24 -0600

    The field kills people

--- Assets/Scripts/WallCollision.cs
+++ Assets/Scripts/WallCollision.cs
@@ -7,9 +7,23 @@ using UnityEngine.Networking;
 [System.Obsolete]
 public class WallCollision : NetworkBehaviour
 {
-    [ServerCallback]
-    private void OnCollisionEnter(Collision other) {
-        Debug.Log("COLLIDE");
-        NetworkServer.Destroy(other.gameObject);
+    [SerializeField] bool destroyOnExit = false;
+    private void OnTriggerEnter(Collider other) {
+        if(destroyOnExit) return;
+        DestroyPlayer(other.gameObject);
+
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(!destroyOnExit) return;
+        DestroyPlayer(other.gameObject);
+        
+    }
+
+    private void DestroyPlayer(GameObject obj){
+        Debug.Log(obj.name);
+        if(obj.tag == "Player"){
+            NetworkAnimator.Destroy(obj);
+        }
     }
 }
