3b233f5ff110d67c8214a6024da89f683f9f8861
unknown <Robert.Aron.tx@gmail.com>
2020-01-19 23:21:15 -0600

    added very basic computer

---
+++ Assets/Scripts/ComputerPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerPlayer : MonoBehaviour
+{
+    [SerializeField] PlayerMovement playerMovement;
+    bool turnLeft = false;
+
+    private void Update() {
+        int shouldFlipTurn = Random.Range(0,2);
+        if(shouldFlipTurn==0) turnLeft = !turnLeft;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        playerMovement.Turn(turnLeft);
+    }
+}
