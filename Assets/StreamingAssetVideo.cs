6e9fd280f1dbb4801ad5b9e79cf10e48fc2ba35a
unknown <Robert.Aron.tx@gmail.com>
2020-08-02 14:42:16 -0500

    help video

---
+++ Assets/StreamingAssetVideo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class StreamingAssetVideo : MonoBehaviour
+{
+    [SerializeField] VideoPlayer videoPlayer = default;
+
+    void Start(){
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"Tutorial_3.webm");
+        videoPlayer.Play();
+    }
+}
