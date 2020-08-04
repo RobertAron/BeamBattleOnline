0d766d992df36a4f2b7c875e45a643b46d2e2760
unknown <Robert.Aron.tx@gmail.com>
2020-08-03 22:52:11 -0500

    i can't beat the video problem

--- Assets/StreamingAssetVideo.cs
+++ Assets/StreamingAssetVideo.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class StreamingAssetVideo : MonoBehaviour
 {
     [SerializeField] VideoPlayer videoPlayer = default;
-
-    void Start(){
+    void Awake()
+    {
         videoPlayer.source = VideoSource.Url;
-        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"Tutorial_3.webm");
-        videoPlayer.prepareCompleted += (UnityEngine.Video.VideoPlayer vPlayer) =>{
-            vPlayer.Play();
-        };
-        videoPlayer.Prepare();
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Tutorial_3.webm");
     }
 }
