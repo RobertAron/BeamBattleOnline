874fbf7b0552931eb8126a7635b043f404743179
unknown <Robert.Aron.tx@gmail.com>
2020-08-11 20:57:43 -0500

    text buttons and info on exit

--- Assets/Scripts/GameScene/UI/MatchPlayerPrefColor.cs
+++ Assets/Scripts/GameScene/UI/MatchPlayerPrefColor.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class MatchPlayerPrefColor : MonoBehaviour
 {
     [SerializeField] TMP_Text tMP_Text = default;
+    [SerializeField] Button button = default;
     PlayerPrefsController playerPrefsController = new PlayerPrefsController();
     // Start is called before the first frame update
     void Start()
     {   
         tMP_Text.color = playerPrefsController.accentColor;
+        var colors = button.colors;
+        colors.highlightedColor = playerPrefsController.accentColor;
+        colors.pressedColor =  playerPrefsController.accentColor;
+        button.colors = colors;
     }
 }
